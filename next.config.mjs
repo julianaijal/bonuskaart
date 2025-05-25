db8567b5a4039f98bf875ae5b81b91ae97ae838d
Julian Aijal <julian.aijal@gmail.com>
2025-05-25 14:15:30 +0200

    add bundle-analyzer

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+import nextBundleAnalyzer from '@next/bundle-analyzer';
+
+const withBundleAnalyzer = nextBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // No image config needed if using local images
+};
+
+export default withBundleAnalyzer(nextConfig);
